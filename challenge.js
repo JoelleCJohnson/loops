652444e96c458e79da403f39a5f1c6de198a85e2
Joelle  Johnson <joellej221@gmail.com>
2023-10-10 10:52:44 -0400

    looped through an array

---
+++ challenge.js
@@ -0,0 +1,5 @@
+const videos = ['cats', 'news', 'games', 'sports']
+
+for(let i = 0; i < videos.length; i++){
+    console.log(videos[i])
+}
---
+++ loops.js
@@ -0,0 +1,12 @@
+/* for(let i = 0; i < 3; i++){
+    console.log('Hello')
+}*/
+
+for(let i = 0; i < 4; i++){
+    console.log(i)
+}
+
+const people = ['todd', 'andrew', 'derek']
+for(i = 0; i < people.length; i++){
+    console.log('Hello ' + people[i])
+}
